76f9b944b6b8e67bb93689781da8ccf1dfe8f952
MSHO <lmoa.1998@gmail.com>
2026-03-07 15:55:12 -0500

    UpaoSeeder 1.1

--- database/seeders/UpaoSeeder.php
+++ database/seeders/UpaoSeeder.php
@@ -18,16 +18,16 @@ class UpaoSeeder extends Seeder
                     'proyecto_tesis' => [
                         'nombre' => 'Plan de Tesis / Proyecto de Investigación',
                         'pasos' => [
-                            ['orden' => 1, 'titulo' => 'Idea de Investigación', 'secciones' => ['Título tentativo', 'Área o línea de investigación']],
-                            ['orden' => 2, 'titulo' => 'Generalidades', 'secciones' => ['Objeto de estudio', 'Duración del proyecto']],
-                            ['orden' => 3, 'titulo' => 'I. Antecedentes', 'instrucciones' => 'Mínimo 5 internacionales y 5 nacionales (últimos 5 años).'],
-                            ['orden' => 4, 'titulo' => 'II. Marco Teórico', 'instrucciones' => 'Bases teóricas y científicas sustentadas.'],
-                            ['orden' => 5, 'titulo' => 'III. Problema', 'secciones' => ['Realidad Problemática', 'Enunciado del Problema']],
-                            ['orden' => 6, 'titulo' => 'IV. Hipótesis', 'instrucciones' => 'Formulación de hipótesis general y específicas.'],
-                            ['orden' => 7, 'titulo' => 'V. Objetivos', 'secciones' => ['Objetivo General', 'Objetivos Específicos']],
-                            ['orden' => 8, 'titulo' => 'VI. Metodología', 'secciones' => ['Tipo y Diseño', 'Población y Muestra', 'Técnicas e Instrumentos']],
-                            ['orden' => 9, 'titulo' => 'VII. Cronograma y VIII. Presupuesto', 'tipo' => 'planeamiento'],
-                            ['orden' => 10, 'titulo' => 'IX. Bibliografía y X. Anexos', 'norma' => 'APA 7']
+                            ['orden' => 1, 'titulo' => 'I. Antecedentes', 'instrucciones' => 'Mínimo 5 internacionales y 5 nacionales (últimos 5 años).'],
+                            ['orden' => 2, 'titulo' => 'II. Marco Teórico', 'instrucciones' => 'Bases teóricas y científicas sustentadas.'],
+                            ['orden' => 3, 'titulo' => 'III. Problema', 'secciones' => ['Realidad Problemática', 'Enunciado del Problema']],
+                            ['orden' => 4, 'titulo' => 'IV. Hipótesis', 'instrucciones' => 'Formulación de hipótesis general y específicas.'],
+                            ['orden' => 5, 'titulo' => 'V. Objetivos', 'secciones' => ['Objetivo General', 'Objetivos Específicos']],
+                            ['orden' => 6, 'titulo' => 'VI. Metodología', 'secciones' => ['Tipo y Diseño', 'Población y Muestra', 'Técnicas e Instrumentos']],
+                            ['orden' => 7, 'titulo' => 'VII. Cronograma', 'tipo' => 'planeamiento'],
+                            ['orden' => 8, 'titulo' => 'VIII. Presupuesto', 'tipo' => 'planeamiento'],
+                            ['orden' => 9, 'titulo' => 'IX. Bibliografía', 'norma' => 'APA 7'],
+                            ['orden' => 10, 'titulo' => 'X. Anexos', 'norma' => 'APA 7']
                         ]
                     ],
                     'informe_final' => [
